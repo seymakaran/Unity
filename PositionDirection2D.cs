a628c89d623a7035b1fc93624d03bcb1f76703ba
Seyma Karan <seymakaran67@gmail.com>
2022-07-24 01:33:39 +0300

    .

--- PositionDirection2D.cs
+++ PositionDirection2D.cs
@@ -1,6 +1,7 @@
 namespace Karan.Util
 { 
-    public enum PositionDirection2D { Up, Right, Down, Left, RightAndUp, RightAndDown, LeftAndDown, LeftAndUp }
+    public enum PositionDirection2D { Up, Right, Down, Left, RightAndUp, RightAndDown, LeftAndDown, LeftAndUp}
+    public enum Rotate2D { Clockwise, CounterClockwise }
 }
 
 
--- TransformExtensions.cs
+++ TransformExtensions.cs
@@ -7,7 +7,7 @@ namespace Karan.Util.Extensions
         public static void MovePosition2D(this MonoBehaviour mb, PositionDirection2D direction, float posX, float posY)
         {
             var pos = mb.transform.position;
-        
+
             switch (direction)
             {
                 case PositionDirection2D.Up :
@@ -41,6 +41,24 @@ namespace Karan.Util.Extensions
             }
 
             mb.transform.localPosition = pos;
+        }
+
+        public static void Whirl2D(this MonoBehaviour mb, Rotate2D rotate, float speed)
+        {
+            object vector;
+            vector = null;
+
+            switch (rotate)
+            {
+                case Rotate2D.Clockwise:
+                    vector = new Vector3(0, 0, -1);
+                    break;
+                case Rotate2D.CounterClockwise :
+                    vector = new Vector3(0, 0, 1);
+                    break;
+            }
+            
+            mb.transform.Rotate((Vector3)vector * Time.deltaTime * speed);
         }
     }
 }
